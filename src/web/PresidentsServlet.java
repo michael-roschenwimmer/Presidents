85e73ef8f7f89d7c573a6288236250fb28179f1f
Sebastian Hellsing <sebastsing@gmail.com>
2017-05-12 16:30:49 -0600

    Created working landing page for site.

--- src/web/PresidentsServlet.java
+++ src/web/PresidentsServlet.java
@@ -1,8 +1,27 @@
 package web;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("Presidents")
 public class PresidentsServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("Welcome.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
 	
 	
 	
